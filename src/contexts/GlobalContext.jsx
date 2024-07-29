388b96f7d4870f4f7d7b664c49fb7bd1abb7c582
Tushar <tusharmohan0416@gmail.com>
2024-07-29 07:07:46 +0530

    weather bug fixed

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react"
+import { createContext, useEffect } from "react"
 import EMPLOYEES_DATA from "../data/employees";
 import { useState } from "react";
 
@@ -8,21 +8,75 @@ export const GlobalContext = createContext();
 export default function GlobalState({ children }) {
 
     const [employees, setEmployees] = useState(EMPLOYEES_DATA);
+
+    // Weather States
+    const [wd, setWd] = useState();
+    const [weatherErrorMsg, setweatherErrorMsg] = useState();
+    const [wloading, setwLoading] = useState(false);
+
+    // Weather Context
+    const lat = 30.365771;
+    const lon = 76.767454;
+    const apiKey = '7088d3220ea90449ffe6aa3bda4c8dfa';
     const [showd, setShowd] = useState({
         cityName: '',
         temp: '',
+        feels_like: '',
         maxTemp: '',
         minTemp: '',
         humidity: '',
         description: '',
     });
 
+    async function fetchWeatherData() {
+        try {
+            setwLoading(true);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+            const data = await response.json();
+
+            if (response.ok) {
+                setWd(data);
+                console.log(data);
+                setwLoading(false);
+            } else {
+                setwLoading(data.message || 'Error fetching data');
+                setwLoading(false);
+            }
+
+        } catch (error) {
+            setweatherErrorMsg('The error is ' + error.message);
+            setwLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchWeatherData();
+    }, []);
+
+    useEffect(() => {
+        if (wd) {
+            setShowd({
+                cityName: wd.name,
+                temp: wd.main.temp,
+                feels_like: wd.main.feels_like,
+                maxTemp: wd.main.temp_max,
+                minTemp: wd.main.temp_min,
+                humidity: wd.main.humidity,
+                description: wd.weather[0].description,
+            });
+        }
+    }, [wd]);
+
+
+
     return (
         <GlobalContext.Provider value={{
             employees,
             setEmployees,
             showd,
-            setShowd
+            setShowd,
+            weatherErrorMsg,
+            wloading
         }}
         >
             {children}
--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -2,52 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../contexts/GlobalContext";
 
 export default function Weather() {
-    const { showd, setShowd } = useContext(GlobalContext);
-    const [wd, setWd] = useState();
-    const [errorMsg, setErrorMsg] = useState();
-    const [loading, setLoading] = useState(false);
-
-    const lat = 30.365771;
-    const lon = 76.767454;
-    const apiKey = '7088d3220ea90449ffe6aa3bda4c8dfa';
-
-    async function fetchWeatherData() {
-        try {
-            setLoading(true);
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
-            const data = await response.json();
-
-            if (response.ok) {
-                setWd(data);
-                console.log(data);
-                setLoading(false);
-            } else {
-                setErrorMsg(data.message || 'Error fetching data');
-                setLoading(false);
-            }
-        } catch (error) {
-            setErrorMsg('The error is ' + error);
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
-        fetchWeatherData();
-    }, []);
-
-    useEffect(() => {
-        if (wd) {
-            setShowd({
-                cityName: wd.name,
-                temp: wd.main.temp,
-                feels_like: wd.main.feels_like,
-                maxTemp: wd.main.temp_max,
-                minTemp: wd.main.temp_min,
-                humidity: wd.main.humidity,
-                description: wd.weather[0].description,
-            });
-        }
-    }, [wd]);
+    const { showd, weatherErrorMsg, wloading } = useContext(GlobalContext);
 
     return (
         <section id="weather-section" className="h-full pb-5 flex flex-col">
@@ -55,14 +10,14 @@ export default function Weather() {
                 <h1 className="text-4xl mt-3 font-bold mb-4">Weather Forecast</h1>
             </div>
             <div className="weather-cont-main mt-2 pt-2 px-5 pb-7 flex flex-col items-center gap-7 rounded-md overflow-y-scroll overflow-x-hidden">
-                {loading ? (
+                {wloading ? (
                     <div className="text-center">
                         <p className="text-lg font-medium">Loading Data... Please wait</p>
                     </div>
                 ) : (
                     <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-                        {errorMsg ? (
-                            <p className="text-red-500 font-medium">{errorMsg}</p>
+                        {weatherErrorMsg ? (
+                            <p className="text-red-500 font-medium">{weatherErrorMsg}</p>
                         ) : (
                             <div className="space-y-4">
                                 <p className="text-2xl font-bold">City Name: {showd.cityName}</p>
